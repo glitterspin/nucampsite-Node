689ee7df5c22b640aea944ba09794ab6e3922d56
Kyle Weisse <kyle.weisse@gmail.com>
2021-08-06 21:32:00 -0400

    new user signup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../models/user');
+
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//allows a new user to register
+router.post('/signup'), (req, res, next) => {
+  User.findOne({username: req.body.username})
+  .then(user => {
+    if (user) {
+      const err = new Error(`User ${req.body.username} already exists!`);
+      err.status = 403;
+      return next(err);
+    } else {
+      User.create({
+        username: req.body.username,
+        password: req.body.password
+      })
+      .then(user => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({status: 'Registration Successful!', user: user});
+      })
+      .catch(err => next(err));
+    }
+  })
+  .catch(err => next(err));
+}
 module.exports = router;
