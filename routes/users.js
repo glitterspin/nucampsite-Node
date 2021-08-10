c33f3afb207f249c02e0c5837ffd2fe2fd1fc856
Kyle Weisse <kyle.weisse@gmail.com>
2021-08-10 12:37:37 -0400

    fixed typo

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) {
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   User.find()
   .populate('comments.author')
   .then(users => {
