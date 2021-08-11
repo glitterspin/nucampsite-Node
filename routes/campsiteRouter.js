658a94c8c9e480b1af5fe70cccbeddad06d2ca37
Kyle Weisse <kyle.weisse@gmail.com>
2021-08-11 11:03:04 -0400

    user check behavior added to comment delete

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -199,24 +199,30 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
 .delete(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
-        if (campsite && campsite.comments.id(req.params.commentId)) {
-            campsite.comments.id(req.params.commentId).remove();
-            campsite.save()
-            .then(campsite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(campsite);
-            })
-            .catch(err => next(err));
-         } else if (!campsite){
-             err = new Error(`Campsite ${req.params.campsiteId} not found`);
-             err.status = 404;
-             return next(err);
-         } else {
-             err = new Error(`Comment ${req.params.commentId} not found`);
-             err.status = 404;
-             return next(err);
-         }
+        if (req.user._id.equals(req.params.commentId).author._id){
+            if (campsite && campsite.comments.id(req.params.commentId)) {
+                campsite.comments.id(req.params.commentId).remove();
+                campsite.save()
+                .then(campsite => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(campsite);
+                })
+                .catch(err => next(err));
+             } else if (!campsite){
+                 err = new Error(`Campsite ${req.params.campsiteId} not found`);
+                 err.status = 404;
+                 return next(err);
+             } else {
+                 err = new Error(`Comment ${req.params.commentId} not found`);
+                 err.status = 404;
+                 return next(err);
+             }
+        } else {
+           err = new Error ('You are not authorized to edit this comment.');
+           err.status = 403;
+           return next(err);
+        }
     })
     .catch(err => next(err));
 });
